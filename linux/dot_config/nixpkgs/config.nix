6f7035538f34e791a65b023730958a23edd33c6e
Bernardo Mesko <bemesko@gmail.com>
2024-08-19 09:13:06 -0300

    Add Nix env configuration

---
+++ linux/dot_config/nixpkgs/config.nix
@@ -0,0 +1,27 @@
+# from https://nixos.org/manual/nixpkgs/stable/#sec-declarative-package-management
+{
+  allowUnfree = true;
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "my-packages";
+      paths = [
+        azure-cli
+        chezmoi
+        docker
+        fzf
+        jqp
+        k9s
+        krew
+        kubectl
+        kubectx
+        kubernetes-helm
+        neovim
+        oh-my-posh
+        python3
+        terraform
+        tmux
+      ];
+      pathsToLink = [ "/share" "/bin" ];
+    };
+  };
+}
