df2a0c7ab1b68d70101e99625afbdbedb3bef983
Bernardo Pires Mesko <bernardo@NTBK-BH7481.bhz.vettatech.com>
2024-11-05 12:11:35 -0300

    disable unnecessary things in markdown files

--- linux/dot_config/nvim/lua/config/autocmds.lua
+++ linux/dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "markdown", "txt" },
+
+	callback = function()
+		vim.b.autoformat = false
+		vim.diagnostic.enable(false)
+		vim.opt_local.spell = false
+	end,
+})
